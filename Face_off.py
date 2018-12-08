2cbaf9961d6716126977ce2f88248995d1d55d0f
Hazem Gabr <hazemgabr232@gmail.com>
2018-12-08 17:30:20 +0200

    Add files via upload

--- Face_off.py
+++ Face_off.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 
 subjects = ["", "Hazem", "Shimaa"]
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 
 
@@ -166,19 +167,46 @@ print("Total labels: ", len(labels))
 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 face_recognizer.train(faces, np.array(labels))
-print("Predicting images...")
+names = ['None', 'Marcelo', 'Paula', 'Ilza', 'Z', 'W']
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
+# Define min window size to be recognized as a face
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
+while True:
+    ret, img = cam.read()
+    img = cv2.flip(img, 1)  # Flip vertically
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier('/home/hazem/opencv-3.2.0/data/lbpcascades/lbpcascade_frontalface.xml')
 
-# load test images
-test_img1 = cv2.imread("/home/hazem/Pictures/8.jpg")
-#test_img2 = cv2.imread("/home/hazem/Pictures/7.jpg")
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        id, confidence = face_recognizer.predict(gray[y:y + h, x:x + w])
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (confidence < 100):
+            id = subjects[id]                             ##if you want to send id as a number make this line a comment
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "0"
+            confidence = "  {0}%".format(round(100 - confidence))
 
-# perform a prediction
-predicted_img1 = predict(test_img1)
-#predicted_img2 = predict(test_img2)
-print("Prediction complete")
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
-# display both images
-cv2.imshow("image", predicted_img1)
-#cv2.imshow(subjects[2], predicted_img2)
-cv2.waitKey(0)
+    cv2.imshow('camera', img)
+    ##send id to the cloud here
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+    if k == 27:
+        break
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
 cv2.destroyAllWindows()
